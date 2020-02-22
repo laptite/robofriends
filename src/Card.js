aa68f2591e92a432c9f11d47c1847aaeb8874e05
Alisa Chang <laptite@gmail.com>
2020-02-22 16:10:06 -0700

    Create Card Component with static robot

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div>
+			<img alt="robot" src="https://robohash.org/test?200x200" />
+			<div>
+				<h2>Robot Name</h2>
+				<p>robot.name@mail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
