12b3134553f39c11f7f69479ce7c1aa8d4e985d5
Alisa Chang <laptite@gmail.com>
2020-02-24 14:43:45 -0700

    Fix extraneous parenthesis

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ErrorBoundry extends Component ({
+class ErrorBoundry extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
